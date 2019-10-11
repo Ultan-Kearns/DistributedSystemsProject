aa1a535215b55a86bb842f8f597454af73b2f1ed
Ultan Kearns <ultankearns@gmail.com>
2019-10-12 00:59:25 +0100

    Added helper code

---
+++ src/main/java/ie/gmit/sw/App.java
@@ -0,0 +1,10 @@
+package ie.gmit.sw;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the GRPC Project");
+		System.out.println(Passwords.getNextSalt());
+	}
+
+}
