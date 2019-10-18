cd782dcd393aa00e364dc9fe04ac495b1974841b
ultan kearns <ultankearns@gmail.com>
2019-10-19 00:09:49 +0100

    Got client communicating with server and is now returning the password user id and all items as per spec

--- src/main/java/ie/gmit/ds/PasswordClient.java
+++ src/main/java/ie/gmit/ds/PasswordClient.java
@@ -4,32 +4,59 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.google.protobuf.BoolValue;
-import com.google.protobuf.Empty;
-
+import ie.gmit.ds.passwordGrpc.passwordStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
 public class PasswordClient {
-	private static final Logger logger =
-            Logger.getLogger(PasswordClient.class.getName());
-    private final ManagedChannel channel; 
-    public PasswordClient(String host, int port) {
-    	 channel = ManagedChannelBuilder
-                 .forAddress(host, port)
-                 .usePlaintext()
-                 .build();	}
-    public void shutdown() throws InterruptedException {
-        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-    }
-    private void validatePassword() {
-  
-    }
-    public static void main(String[] args) throws Exception {
-        PasswordClient client = new PasswordClient("localhost", 50551);
-       
-    }
+	private passwordGrpc.passwordStub passwordService;
+	private static final Logger logger = Logger.getLogger(PasswordClient.class.getName());
+	private final ManagedChannel channel;
+
+	public PasswordClient(String host, int port) {
+		channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+		passwordService = passwordGrpc.newStub(channel);
+	}
+
+	public void shutdown() throws InterruptedException {
+		channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+	}
+
+	public void hashPassword(Hash password) {
+		logger.info("Hashing password  " + password);
+		try {
+			StreamObserver<Hash> responseObserver = null;
+			passwordService.hashPassword(password, responseObserver);
+		} catch (StatusRuntimeException ex) {
+			logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+			return;
+		}
+		/*
+		 * if (result.getValue()) { logger.info("Successfully added item " + newItem); }
+		 * else { logger.warning("Failed to add item"); }
+		 */
+	}
+
+	private void validatePassword() {
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		PasswordClient client = new PasswordClient("localhost", 50551);
+		String salt = Passwords.getNextSalt().toString();
+		Hash newPassword = Hash.newBuilder().setUserId(1234).setSalt(salt)
+				.setHashedPassword(Passwords.generateRandomPassword(12) + salt).build();
+		try {
+			if(newPassword != null)
+			{
+				logger.info("New password = " +  newPassword.getAllFields());
+			}
+
+		} finally {
+			// Don't stop process, keep alive to receive async response
+			Thread.currentThread().join();
+		}
+	}
 }
